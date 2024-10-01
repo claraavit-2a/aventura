3baf248450cc5b70d1a55600de1ff72cc88c6a61
claraavit-2a <00001107825702SP@al.educacao.sp.gov.br>
2024-10-01 09:03:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document .querySelectorAll(.btn-proximo);
+
+avanca. forEach(Button=>[
+    button.addevertlistener("click,function")(){
+        const atual=document.querySelector('ativo');
+        const proximo passo = 'passso-" + this.getattribute(data-proximo);
+
+        atual.classlist.remove('ativo');
+        // documento.getelementbyid(proximoásso). classlist.add('ativo');
+    }
+]
+)
